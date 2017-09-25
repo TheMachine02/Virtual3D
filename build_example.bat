cc69cc82ff0e2737dc1a8189e2335b43fa666914
TheMachine02 <themachine02om@gmail.com>
2017-09-25 22:47:25 +0200

    Added example1
    Also fixed raster, pipeline and texture code in backface culling, enforced some code definition.

--- build_example.bat
+++ build_example.bat
@@ -1,6 +1,11 @@
 @echo off
 color 0F
 :Loop
+echo -----------------------------------
+echo Building example.ez80...
 tools\spasm -L -T -E -A -S -I include/ example.ez80 bin/TEST.8xp
+echo -----------------------------------
+echo Building example1.ez80...
+tools\spasm -L -T -E -A -S -I include/ example1.ez80 bin/TEST1.8xp
 pause
 goto Loop
--- build_model.bat
+++ build_model.bat
@@ -2,10 +2,15 @@
 color 0F
 :Loop
 
+echo Converting TONYBOSS_TR2.obj...
 tools\mdlconv  file\TONYBOSS_TR2.obj -T -N -S -B
 tools\spasm file\TONYBOSS_TR20.inc bin\TONY0.8xv -E -S
 tools\spasm file\TONYBOSS_TR21.inc bin\TONY1.8xv -E -S
-pause
+
+echo "Converting Suzanne.obj..."
+tools\mdlconv  file\Suzanne.obj -C -N -S -B
+tools\spasm file\Suzanne0.inc bin\SUZAN0.8xv -E -S
+tools\spasm file\Suzanne1.inc bin\SUZAN1.8xv -E -S
 
 tools\xmlconv
 pause
