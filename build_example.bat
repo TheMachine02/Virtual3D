6144fee6461669841bbc6df73b374c1218f71753
TheMachine02 <themachine02om@gmail.com>
2017-10-03 22:52:38 +0200

    Added matrix lookAt
    Since vxNormalize doesn't exist, the routine isn't functionnal *yet*

--- build_example.bat
+++ build_example.bat
@@ -7,5 +7,8 @@ tools\spasm -L -T -E -A -S -I include/ example.ez80 bin/TEST.8xp
 echo -----------------------------------
 echo Building example1.ez80...
 tools\spasm -L -T -E -A -S -I include/ example1.ez80 bin/TEST1.8xp
+echo -----------------------------------
+echo Building example1.ez80...
+tools\spasm -L -T -E -A -S -I include/ example2.ez80 bin/TEST2.8xp
 pause
 goto Loop
