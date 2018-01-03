7eb04c0c8613ce2c49c6b64d9f9d585e838e9fac
TheMachine02 <themachine02om@gmail.com>
2018-01-03 18:11:58 +0100

    Removed spasm and try convhex

--- build_example.bat
+++ build_example.bat
@@ -3,18 +3,20 @@ color 0F
 :Loop
 echo -----------------------------------
 echo Building example.ez80...
-tools\spasm -L -T -E -A -S -I include/ -I lib/ example.ez80 bin/TEST.8xp
+spasm -L -T -E -A -S -I include/ -I lib/ example.ez80 bin/TEST.8xp
 echo -----------------------------------
 echo Building example1.ez80...
-tools\spasm -L -T -E -A -S -I include/ -I lib/ example1.ez80 bin/TEST1.8xp
+spasm -L -T -E -A -S -I include/ -I lib/ example1.ez80 bin/TEST1.8xp
 echo -----------------------------------
 echo Building example2.ez80...
-tools\spasm -L -T -E -A -S -I include/ -I lib/ example2.ez80 bin/TEST2.8xp
+spasm -L -T -E -A -S -I include/ -I lib/ example2.ez80 bin/TEST2.bin
+convhex -x bin\TEST2.bin
+del bin\TEST2.bin
 echo -----------------------------------
 echo Building alpha.vsl...
-tools\spasm -L -T -E -A -S -I include/ -I lib/ alpha.vsl bin/PXLSHADER1.8xv
+spasm -L -T -E -A -S -I include/ -I lib/ alpha.vsl bin/PXLSHADER1.8xv
 echo -----------------------------------
 echo Building light.vsl...
-tools\spasm -L -T -E -A -S -I include/ -I lib/ light.vsl bin/PXLSHADER0.8xv
+spasm -L -T -E -A -S -I include/ -I lib/ light.vsl bin/PXLSHADER0.8xv
 pause
 goto Loop
--- build_texture.bat
+++ build_texture.bat
@@ -10,10 +10,10 @@ tools\texconv -A file\mateus.png
 tools\texconv -A file\chocobo.png
 tools\texconv -D file\Tonberry.png
 
-tools\spasm -I lib/ file\m1034.inc bin\FLUFFY3.8xv -E -S
-tools\spasm -I lib/ file\fran.inc bin\FRAN3.8xv -E -S
-tools\spasm -I lib/ file\train.inc bin\TRAIN2.8xv -E -S
-tools\spasm -I lib/ file\bombo.inc bin\BOMBO3.8xv -E -S
-tools\spasm -I lib/ file\mateus.inc bin\MATEUS2.8xv -E -S
-tools\spasm -I lib/ file\chocobo.inc bin\CHOCOBO3.8xv -E -S
-tools\spasm -I lib/ file\Tonberry.inc bin\TONB3.8xv -E -S
+spasm -I lib/ file\m1034.inc bin\FLUFFY3.8xv -E -S
+spasm -I lib/ file\fran.inc bin\FRAN3.8xv -E -S
+spasm -I lib/ file\train.inc bin\TRAIN2.8xv -E -S
+spasm -I lib/ file\bombo.inc bin\BOMBO3.8xv -E -S
+spasm -I lib/ file\mateus.inc bin\MATEUS2.8xv -E -S
+spasm -I lib/ file\chocobo.inc bin\CHOCOBO3.8xv -E -S
+spasm -I lib/ file\Tonberry.inc bin\TONB3.8xv -E -S
