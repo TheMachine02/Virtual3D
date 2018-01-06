371af323964d8d2e817bfc169ad33e5c40fdd550
TheMachine02 <themachine02om@gmail.com>
2018-01-06 22:41:47 +0100

    New build parameters

--- build_example.bat
+++ build_example.bat
@@ -3,10 +3,14 @@ color 0F
 :Loop
 echo -----------------------------------
 echo Building example.ez80...
-spasm -L -T -E -A -S -I include/ -I lib/ example.ez80 bin/TEST.8xp
+spasm -L -T -E -A -S -I include/ -I lib/ example.ez80 bin/TEST.bin
+convhex -x bin\TEST.bin
+del bin\TEST.bin
 echo -----------------------------------
 echo Building example1.ez80...
-spasm -L -T -E -A -S -I include/ -I lib/ example1.ez80 bin/TEST1.8xp
+spasm -L -T -E -A -S -I include/ -I lib/ example1.ez80 bin/TEST1.bin
+convhex -x bin\TEST1.bin
+del bin\TEST1.bin
 echo -----------------------------------
 echo Building example2.ez80...
 spasm -L -T -E -A -S -I include/ -I lib/ example2.ez80 bin/TEST2.bin
