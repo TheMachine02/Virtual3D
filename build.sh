35ff2feca6d54888122ebf804ff8fe05251c4644
blossom <>
2020-11-06 19:29:00 +0100

    More and more cleaning

--- build.sh
+++ build.sh
@@ -1,13 +1,13 @@
 mkdir bin
 echo "-----------------------------------"
 echo "Building example.ez80..."
-spasm -L -T -E -A -S -I include/ -I lib/ example.ez80 bin/TEST.8xp
+spasm -L -T -E -A -S -I include/ -I lib/ example/example.ez80 bin/TEST.8xp
 echo "-----------------------------------"
 echo "Building example1.ez80..."
-spasm -L -T -E -A -S -I include/ -I lib/ example1.ez80 bin/TEST1.8xp
+spasm -L -T -E -A -S -I include/ -I lib/ example/example1.ez80 bin/TEST1.8xp
 echo "-----------------------------------"
 echo "Building example2.ez80..."
-spasm -L -T -E -A -S -I include/ -I lib/ example2.ez80 bin/TEST2.8xp
+spasm -L -T -E -A -S -I include/ -I lib/ example/example2.ez80 bin/TEST2.8xp
 echo "-----------------------------------"
 echo "Building lvl.ez80..."
-spasm -L -T -E -A -S -I include/ -I lib/ lvl.ez80 bin/TESTLVL.8xp
+spasm -L -T -E -A -S -I include/ -I lib/ example/lvl.ez80 bin/TESTLVL.8xp
---
+++ lib/shader/vxAlphaShader.asm
@@ -0,0 +1,40 @@
+#include	"vxVSL.inc"
+
+alphaShader:
+	.db	VX_PIXEL_SHADER		; shader type
+	.dl	alphaEnd-alphaCode	; shader size
+	.db	2			; two pixel are written per loop
+	.db	16			; total size of per pixel code
+.relocate	VX_PIXEL_SHADER_CODE
+alphaCode:
+	add	hl, de
+	ld	a, h
+	exx
+	adc	hl, sp
+	ld	h, a
+	ld	b, (hl)			; fetch the texture
+	ld	a, (de)			; fetch the fb color
+	ld	c, a			;
+	ld	a, (bc)			;
+	set	7, h
+	add	a, (hl)			; add up both color
+	ld	(de), a			; and write it to fb
+	inc	de
+	exx
+	add	hl, de
+	ld	a, h
+	exx
+	adc	hl, sp
+	ld	h, a
+	ld	b, (hl)			; fetch the texture
+	ld	a, (de)			; fetch the fb color
+	ld	c, a			;
+	ld	a, (bc)			;
+	set	7, h
+	add	a, (hl)			; add up both color
+	ld	(de), a			; and write it to fb
+	inc	de
+	exx
+	djnz	alphaCode
+alphaEnd:
+.endrelocate
