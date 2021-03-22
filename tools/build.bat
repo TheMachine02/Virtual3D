b2c112be204097a2b0dd4ea8aff94fcc4d2b162c
TheMachine02 <>
2021-03-22 15:34:55 +0100

    build: better compile flags

--- tools/build.bat
+++ tools/build.bat
@@ -1,4 +1,13 @@
+@echo off
+@cls
+color 0F
+
+:Loop
+
 g++ -o mdlconv mdlconv-src/main.cpp -fexceptions -fpie -fstack-clash-protection -Wall -O2 -pipe -D_FORTIFY_SOURCE=2
 g++ -o texconv texconv-src/main.cpp -fexceptions -fpie -fstack-clash-protection -Wall -O2 -pipe -D_FORTIFY_SOURCE=2
 g++ -o xmlconv xmlconv-src/main.cpp -fexceptions -fpie -fstack-clash-protection -Wall -O2 -pipe -D_FORTIFY_SOURCE=2
 g++ -o lut lut.cpp -fexceptions -fpie -fstack-clash-protection -Wall -O2 -pipe -D_FORTIFY_SOURCE=2
+
+pause
+goto Loop
